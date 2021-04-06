79c1eb790d3225c27856c9cbb6d4dea4b157a12a
AhmedTitu <81875524+AhmedTitu@users.noreply.github.com>
2021-04-06 11:56:03 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,113 +1,151 @@
-const luckyNoDisplay = document.querySelector('#winningNoDis');
-const luckyNoBtn = document.querySelector('#luckyNoBtn');
-const winnerNameDisplay = document.querySelector('#winnerName');
-const player1GuessNoDisplay = document.querySelector('#player1GuessNo');
-const player2GuessNoDisplay = document.querySelector('#player2GuessNo');
-const player1Btn = document.querySelector('#player1Btn');
-const player2Btn = document.querySelector('#player2Btn');
-const resetBtn = document.querySelector('#resetBtn');
-const congratulationImg = document.querySelector('#congratulation');
+;(function () {
+  const luckyNoDisplay = document.querySelector('#winningNoDis')
+  const luckyNoBtn = document.querySelector('#luckyNoBtn')
+  const winnerNameDisplay = document.querySelector('#winnerName')
+  const player1GuessNoDisplay = document.querySelector('#player1GuessNo')
+  const player2GuessNoDisplay = document.querySelector('#player2GuessNo')
+  const player1Btn = document.querySelector('#player1Btn')
+  const player2Btn = document.querySelector('#player2Btn')
+  const resetBtn = document.querySelector('#resetBtn')
+  const congratulationImg = document.querySelector('#congratulation')
 
-congratulationImg.style.display = "none";
+  congratulationImg.style.display = 'none'
 
-let luckyNumber= 0;
-let player1Number = 0;
-let player2Number = 0;
-let p1Clicked = true;
-let p2Clicked = true;
+  let luckyNumber = null
 
-let player1Span = 0 ;
-let player2Span = 0 ;
-
-    function playerSpanFunc(){
-        if (player1Span === 5){
-            alert('Game Over') , reset()
-
-        } else if (player2Span === 5){
-            alert('Game Over') , reset()
-        }
-    };
-
-
-    function winner(oldScore , luckyNumber){
-        if(oldScore === luckyNumber){
-            if(player1Number === luckyNumber && player1Number > 1){
-                winnerNameDisplay.textContent = 'Player One'
-                luckyNoBtn.setAttribute('disabled' , 'disabled')
-                player1Btn.setAttribute('disabled' , 'disabled')
-                player2Btn.setAttribute('disabled' , 'disabled')
-                congratulationImg.style.display = "block"
-                
-            }else if (player2Number === luckyNumber && player2Number > 1){
-                winnerNameDisplay.textContent = 'Player Two'
-                luckyNoBtn.setAttribute('disabled' , 'disabled')
-                player1Btn.setAttribute('disabled' , 'disabled')
-                player2Btn.setAttribute('disabled' , 'disabled')
-                congratulationImg.style.display = "block"
-                } 
-            }
-        };
-
-    function reset(){
-        luckyNumber = 0
-        player1Number = 0
-        player2Number = 0
-        winnerNameDisplay.textContent = 'Please Continue Trying'
-        luckyNoDisplay.textContent = "?"
-        player1GuessNoDisplay.textContent = "?"
-        player2GuessNoDisplay.textContent = "?"
-        luckyNoBtn.removeAttribute('disabled')
-        player1Btn.removeAttribute('disabled')
-        player2Btn.removeAttribute('disabled')
-        congratulationImg.style.display = "none"
-    };
-
-    function validation(){
-        if (luckyNumber === 0){
-         alert('At First You Have To Get A Lucky Number') , reset()
-        }
-    };
-
-    function fareGame1(){
-        if(p1Clicked === false){
-            player1Btn.setAttribute('disabled' , 'disabled')
-            player2Btn.removeAttribute('disabled')
-         }
-    };
-         
-    function fareGame2(){
-        if(p2Clicked === false){
-        player2Btn.setAttribute('disabled' , 'disabled')
-        player1Btn.removeAttribute('disabled')
-        // p1Clicked = true
-        }
-    };
-
-luckyNoBtn.addEventListener('click' , () => {
-    luckyNumber = Math.floor(Math.random()* 10 ) + 1
-    luckyNoDisplay.textContent = luckyNumber
+  function player(number = 0, clicked = false, lifespan = 5) {
+    return {
+      number,
+      clicked,
+      lifespan
     }
-);
+  }
 
-player1Btn.addEventListener('click' , () => {
-    player1Number = Math.floor(Math.random() * 10) + 1
-    player1GuessNoDisplay.textContent = player1Number
-    p1Clicked = !p1Clicked
-    winner(player1Number , luckyNumber)
-    validation()
-    fareGame1()
-    player1Span++;
-    playerSpanFunc()
-});
+  const player1 = player()
+  const player2 = player()
 
-player2Btn.addEventListener('click' , () => {
-    player2Number = Math.floor(Math.random() * 10) + 1
-    player2GuessNoDisplay.textContent = player2Number
-    p2Clicked = !p2Clicked
-    winner(player2Number , luckyNumber)
-    validation()
-    fareGame2()
-    player2Span++;
-    playerSpanFunc()
-});
-resetBtn.addEventListener('click', reset)
+  //after finishing the chance of players, automatically reset the game
+  function getDefaultPlayState() {
+    if (player1.lifespan === 0 || player2.lifespan === 0) {
+      alert('No One wins, Play Again')
+      //Reset the game
+      reset()
+    }
+  }
+
+  function checkWinner(playerScore, luckyNumber, player) {
+    if (playerScore === luckyNumber && player === 'player1') {
+      winnerNameDisplay.textContent = 'Player One'
+      luckyNoBtn.setAttribute('disabled', 'disabled')
+      player1Btn.setAttribute('disabled', 'disabled')
+      player2Btn.setAttribute('disabled', 'disabled')
+      congratulationImg.style.display = 'block'
+    } else if (playerScore === luckyNumber && player === 'player2') {
+      winnerNameDisplay.textContent = 'Player Two'
+      luckyNoBtn.setAttribute('disabled', 'disabled')
+      player1Btn.setAttribute('disabled', 'disabled')
+      player2Btn.setAttribute('disabled', 'disabled')
+      congratulationImg.style.display = 'block'
+    }
+  }
+
+  function reset() {
+    luckyNumber = null
+    player1.number = 0
+    player2.number = 0
+    player1.lifespan = 5
+    player2.lifespan = 5
+    winnerNameDisplay.textContent = 'Please Continue Trying'
+    luckyNoDisplay.textContent = '?'
+    player1GuessNoDisplay.textContent = '?'
+    player2GuessNoDisplay.textContent = '?'
+    luckyNoBtn.removeAttribute('disabled')
+    player1Btn.removeAttribute('disabled')
+    player2Btn.removeAttribute('disabled')
+    congratulationImg.style.display = 'none'
+  }
+
+  function checkLuckyNumBeforePlay() {
+    if (!luckyNumber) {
+      alert('At First You Have To Get A Lucky Number')
+      return false
+    } else {
+      return true
+    }
+  }
+
+  function disablePlayerBtn(player) {
+    if (player === 'player1') {
+      console.log('Running')
+      switch (player1.clicked) {
+        case true:
+          player1Btn.setAttribute('disabled', 'disabled')
+          player2Btn.removeAttribute('disabled')
+          break
+        case false:
+          player1Btn.removeAttribute('disabled')
+          player2Btn.setAttribute('disabled', 'disabled')
+          break
+        default:
+          player1Btn.removeAttribute('disabled')
+          player2Btn.removeAttribute('disabled', 'disabled')
+      }
+    } else {
+      switch (player2.clicked) {
+        case true:
+          player2Btn.setAttribute('disabled', 'disabled')
+          player1Btn.removeAttribute('disabled')
+          break
+        case false:
+          player2Btn.removeAttribute('disabled')
+          player1Btn.setAttribute('disabled', 'disabled')
+          break
+        default:
+          player1Btn.removeAttribute('disabled')
+          player2Btn.removeAttribute('disabled', 'disabled')
+      }
+    }
+  }
+  function pickLuckyNum() {
+    reset()
+    return Math.floor(Math.random() * 10) + 1
+  }
+
+  luckyNoBtn.addEventListener('click', () => {
+    luckyNumber = pickLuckyNum()
+    luckyNoDisplay.textContent = luckyNumber
+  })
+
+  player1Btn.addEventListener('click', () => {
+    const isLuckyNumberDefined = checkLuckyNumBeforePlay()
+    if (!isLuckyNumberDefined) return
+
+    player1.number = Math.floor(Math.random() * 10) + 1
+    player1GuessNoDisplay.textContent = player1.number
+
+    player1.clicked = true
+
+    player1.lifespan--
+    getDefaultPlayState()
+
+    disablePlayerBtn('player1')
+    checkWinner(player1.number, luckyNumber, 'player1')
+  })
+
+  player2Btn.addEventListener('click', () => {
+    const isLuckyNumberDefined = checkLuckyNumBeforePlay()
+    if (!isLuckyNumberDefined) return
+
+    player2.number = Math.floor(Math.random() * 10) + 1
+    player2GuessNoDisplay.textContent = player2.number
+    player2.clicked = true
+    player2.lifespan--
+
+    disablePlayerBtn('player2')
+    getDefaultPlayState()
+
+    checkWinner(player2.number, luckyNumber, 'player2')
+  })
+
+  resetBtn.addEventListener('click', reset)
+})()
